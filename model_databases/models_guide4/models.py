1dec3d7053722759878ea05e8ed76d97488e6a88
Aldin Ugljanin <aldinugljaninn@gmail.com>
2023-11-17 21:09:11 +0100

    Creating custom fields and validating data

--- model_databases/models_guide4/models.py
+++ model_databases/models_guide4/models.py
@@ -1,4 +1,17 @@
+from typing import Any
 from django.db import models
 from django.core.exceptions import ValidationError
 
 # Create your models here.
+def int_is_evean(value):
+    if (value % 2 != 0):
+        raise ValidationError(str(value) + " is not even.")
+
+class EvenInteger(models.IntegerField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.validators.append(int_is_evean)
+
+class MyModel(models.Model):
+    # even_int = models.IntegerField(validators=[int_is_evean])
+    even_int = EvenInteger()
